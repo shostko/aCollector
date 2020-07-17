431c9ad6be7a4a589f1a9823a3047589529170fa
Sergey Shostko <s.shostko@gmail.com>
2020-07-17 13:39:38 +0300

    add firebase-analytics Collector

---
+++ firebase-analytics/src/main/java/by/shostko/acollector/FirebaseAnalyticsCollector.kt
@@ -0,0 +1,55 @@
+package by.shostko.acollector
+
+import android.app.Activity
+import android.content.Context
+import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
+
+class FirebaseAnalyticsCollector(context: Context) : Collector {
+
+    private val firebase: FirebaseAnalytics = FirebaseAnalytics.getInstance(context)
+
+    override fun reset() {
+        firebase.resetAnalyticsData()
+    }
+
+    override fun setEnabled(enabled: Boolean) {
+        firebase.setAnalyticsCollectionEnabled(enabled)
+    }
+
+    override fun track(event: String, data: Bundle?) {
+        firebase.logEvent(event, data)
+    }
+
+    override fun setScreen(activity: Activity, name: String?, clazz: Class<*>?) {
+        firebase.setCurrentScreen(activity, name, clazz?.simpleName)
+    }
+
+    override fun setUser(identifier: String?) {
+        firebase.setUserId(identifier)
+    }
+
+    override fun setProperty(key: String, value: String?) {
+        firebase.setUserProperty(key, value)
+    }
+
+    override fun setProperty(key: String, value: Boolean?) {
+        setProperty(key, value?.toString())
+    }
+
+    override fun setProperty(key: String, value: Double?) {
+        setProperty(key, value?.toString())
+    }
+
+    override fun setProperty(key: String, value: Float?) {
+        setProperty(key, value?.toString())
+    }
+
+    override fun setProperty(key: String, value: Int?) {
+        setProperty(key, value?.toString())
+    }
+
+    override fun setProperty(key: String, value: Long?) {
+        setProperty(key, value?.toString())
+    }
+}
