8d857a1c8bc2d1846e1cb4cf3458c7fb0ec9b14b
Sergey Shostko <s.shostko@gmail.com>
2020-09-06 15:23:10 +0300

    add mixpanel collector

---
+++ mixpanel/src/main/java/by/shostko/acollector/MixpanelCollector.kt
@@ -0,0 +1,84 @@
+package by.shostko.acollector
+
+import android.app.Activity
+import android.content.Context
+import android.os.Bundle
+import com.mixpanel.android.mpmetrics.MixpanelAPI
+import org.json.JSONObject
+
+open class MixpanelCollector(
+    context: Context,
+    token: String,
+    private val setScreenEvent: String = SCREEN
+) : Collector {
+
+    companion object {
+        private const val SCREEN = "screen"
+        private const val ACTIVITY = "activity"
+        private const val NAME = "name"
+        private const val CLASS = "class"
+    }
+
+    private val mixpanel = MixpanelAPI.getInstance(context, token)
+
+    override fun reset() {
+        mixpanel.reset()
+    }
+
+    override fun setEnabled(enabled: Boolean) {
+        if (enabled) {
+            mixpanel.optInTracking()
+        } else {
+            mixpanel.optOutTracking()
+        }
+    }
+
+    override fun track(event: String, data: Bundle?) {
+        if (data == null || data.isEmpty) {
+            mixpanel.track(event)
+        } else {
+            val props = JSONObject()
+            for (key in data.keySet()) {
+                props.put(key, data[key])
+            }
+            mixpanel.track(event, props)
+        }
+    }
+
+    override fun setScreen(activity: Activity, name: String?, clazz: Class<*>?) {
+        mixpanel.track(setScreenEvent, JSONObject().apply {
+            put(ACTIVITY, activity.javaClass.simpleName)
+            put(NAME, name)
+            put(CLASS, clazz?.simpleName)
+        })
+    }
+
+    override fun setUser(identifier: String?) {
+        mixpanel.identify(identifier)
+        mixpanel.people.identify(identifier)
+    }
+
+    override fun setProperty(key: String, value: String?) {
+        mixpanel.people.set(key, value)
+    }
+
+    override fun setProperty(key: String, value: Boolean?) {
+        mixpanel.people.set(key, value)
+    }
+
+    override fun setProperty(key: String, value: Double?) {
+        mixpanel.people.set(key, value)
+    }
+
+    override fun setProperty(key: String, value: Float?) {
+        mixpanel.people.set(key, value)
+    }
+
+    override fun setProperty(key: String, value: Int?) {
+        mixpanel.people.set(key, value)
+    }
+
+    override fun setProperty(key: String, value: Long?) {
+        mixpanel.people.set(key, value)
+    }
+}
